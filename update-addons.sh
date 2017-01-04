50ab6c643f9e194b46c25fcfc95343d5185f9c59
loriandkkk <kuas1095180129@gmail.com>
2017-01-04 06:27:33 +0000

    add route add 10.0.0.1 on cni0 / flannel.1 and change controller-manager yaml let controller-manager can run rbd script

--- update-addons.sh
+++ update-addons.sh
@@ -3,6 +3,25 @@
 KUBECTL=${KUBECTL_BIN:-/hyperkube kubectl}
 KUBECTL_OPTS=${KUBECTL_OPTS:-}
 
+function dns_route_maintainer(){
+  set -x
+  while true; do
+    if [[ -n "$(ip route | grep -w 'cni0')" ]]; then
+      if [[ -n "$(ip route | grep -w 'flannel.1' | grep -w '10.0.0.10')" ]]; then
+        ip route del 10.0.0.10/32 dev flannel.1
+      fi
+      if [[ -z "$(ip route | grep -w 'cni0' | grep -w '10.0.0.10')" ]]; then
+        ip route add 10.0.0.10/32 dev cni0
+      fi
+    else
+      if [[ -z "$(ip route | grep -w 'flannel.1' | grep -w '10.0.0.10')" ]]; then
+        ip route add 10.0.0.10/32 dev flannel.1
+      fi
+    fi
+    sleep 2;
+  done
+}
+
 function main(){
 
   sed -i "s|^  labels:|  labels:\n    kubernetes.io/cluster-service: 'true'|g" /etc/kubernetes/addons/multinode/heapster/*-controller.yaml
@@ -16,6 +35,8 @@ function main(){
   done
 
   echo "== default service account in the kube-system namespace has token ${token_found} =="
+
+  dns_route_maintainer &
 
   while true; do
 	  sleep 3600;
